3bccbd8b3e054d6134db594808163277efb930ed
Codemis <johnathan@missionaldigerati.com>
2013-01-31 19:48:22 -0800

    Initial Commit

---
+++ Snappy Snap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Snappy Snap
+//
+//  Created by Johnathan Pulos on 1/31/13.
+//  Copyright (c) 2013 Johnathan Pulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Snappy Snap/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Snappy Snap
+//
+//  Created by Johnathan Pulos on 1/31/13.
+//  Copyright (c) 2013 Johnathan Pulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
